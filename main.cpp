93d03658e7a1f204245ff016eac8c402eaacd3ae
HarithaMihimal <harithamihimal4@gmail.com>
2025-10-22 20:03:30 +0530

    commit

--- main.cpp
+++ main.cpp
@@ -1,22 +1,7 @@
 #include <iostream>
 
 int main() {
-    int file_size, counter;
-    file_size = 100;
-    double sales =99.9;
-    std:: cout<<file_size;
-    std::cout << "Hello World";
+    const double pi = 3.14;
 
-
-    int a;
-    int b;
-
-    a=10;
-    b=20;
-    std::cout<<a<<b;
-    int temp =a;
-    a=b;
-    b=temp;
-    std::cout<<a<<b;
     return 0;
 }
