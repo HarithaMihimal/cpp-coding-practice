de29437d07b4d5622dd6de23be3862af3da4af6b
HarithaMihimal <harithamihimal4@gmail.com>
2025-10-22 20:05:35 +0530

    Notation

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 
 int main() {
     const double pi = 3.14;
-
+    int file_size=10; //Snake case
+    int FileSize =10; //Pascal case
+    int fileSize =10; // camel case
+    int iFileSize=10; //Hungarian Notation
     return 0;
 }
