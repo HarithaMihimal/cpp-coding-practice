cf0c26a38d5e8534dbc9a40c9ca40141c065c9d0
HarithaMihimal <harithamihimal4@gmail.com>
2025-10-22 19:55:27 +0530

    swap values  codes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
 int main() {
+    int file_size, counter;
+    file_size = 100;
+    double sales =99.9;
+    std:: cout<<file_size;
     std::cout << "Hello World";
+
+
+    int a;
+    int b;
+
+    a=10;
+    b=20;
+    std::cout<<a<<b;
+    int temp =a;
+    a=b;
+    b=temp;
+    std::cout<<a<<b;
     return 0;
 }
