15880cb1af65ad078ca9818076dd57ae8f1a91b1
HarithaMihimal <harithamihimal4@gmail.com>
2025-10-24 00:23:05 +0530

    commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 
+//int main() {
+//    const double pi = 3.14;
+//    int file_size=10; //Snake case
+//    int FileSize =10; //Pascal case
+//    int fileSize =10; // camel case
+//    int iFileSize=10; //Hungarian Notation
+//    return 0;
+//}
+
 int main() {
-    const double pi = 3.14;
-    int file_size=10; //Snake case
-    int FileSize =10; //Pascal case
-    int fileSize =10; // camel case
-    int iFileSize=10; //Hungarian Notation
+    int x=2;
+    int y=3;
+    int z=x+y;
+    std::cout<<z;
+    std::cout<<"\n";
+    int I = x/y;
+    float II=(float )x/(float)y;
+    std::cout<<I;
+    std::cout<<"\n";
+    std::cout<<II;
     return 0;
 }
